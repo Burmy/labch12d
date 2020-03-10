078f15d45aafdf4341b94d0410a82821fbfa4c64
Burmy <62005549+Burmy@users.noreply.github.com>
2020-03-10 02:22:49 -0700

    Update app.h

--- app.h
+++ app.h
@@ -1,5 +1,4 @@
-#ifndef _app_h_
-#define _app_h_
+#pragma once
 #include "date.h"
 #include "time.h"
 #include <string> 
@@ -10,16 +9,56 @@ private:
 	Date date;
 	Time startTime;
 	Time endTime;
+	Time deriveEndTime();
 
 public:
-	Appointment(std::string, Date, Time, Time);
-
+	/**
+	Constructor
+	Initializes Appointment object
+	*/
+	Appointment();
+	/**
+	Constructor
+	Constructs an appointment
+	@param description
+	@param date
+	@param time
+	*/
+	Appointment(std::string, Date, Time);
+	/**
+	Accessor function to receive description
+	@return description
+	*/
 	std::string getDescription() const;
+	/**
+	Accessor function to receive date
+	@return date
+	*/
 	Date getDate() const;
+	/**
+	Accessor function to receive start time
+	@return startTime
+	*/
 	Time getStart() const;
+	/**
+	Accessor function receive end time
+	@return endTime
+	*/
 	Time getEnd() const;
-
+	/**
+	Boolean function to compare 2 appointments
+	@return true if greater
+	*/
 	bool compare(Appointment);
+	/**
+	Boolean function determining existence of conflict between 2 appointments
+	@return true if greater
+	*/
 	bool conflicts(Appointment);
+
+	/**
+	String function for printing appointments
+	*/
+	std::string toString();
 };
-#endif
+
